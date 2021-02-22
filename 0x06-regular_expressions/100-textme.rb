6feab6cc77127217403cab9a23e1109d81746488
GorillaFu <jared@beguelin.com>
2021-02-22 18:27:13 +0000

    added flag extraction

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/from:(\+?\w+)/).join + "," + ARGV[0].scan(/to:(\+?\w+)/).join
+puts ARGV[0].scan(/from:(\+?\w+)/).join + "," + ARGV[0].scan(/to:(\+?\w+)/).join + "," + ARGV[0].scan(/flags:(\-?\d:\-?\d:\-?\d:\-?\d:\-?\d)/).join + "\n"
